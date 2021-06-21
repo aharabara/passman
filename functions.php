3e1c5a002e8d0968d6351fa19aebbe65f12621b3
Alexandr Harabara <alexandr.harabara@bitpanda.com>
2021-06-21 20:32:42 +0300

    Project cleanup and minor changes

--- functions.php
+++ functions.php
@@ -2,24 +2,88 @@
 
 function get_password(string $fileWithPasswords, string $alias): string
 {
-    return get_passwords($fileWithPasswords, expect_password("Master password: "))[$alias];
+    $passwords = get_passwords($fileWithPasswords, expect_password("Master password: "));
+    if (!isset($passwords[$alias])) {
+        die("This password does not exits.\n");
+    }
+    return $passwords[$alias];
 }
+
+function set_password(string $fileWithPasswords, string $alias): void
+{
+    $masterPass = expect_password("Master password: ");
+    $passwords = get_passwords($fileWithPasswords, $masterPass);
+    if (isset($passwords[$alias])) {
+        if (!get_confirmation('This password already exist. Rewrite? [y/n] ')) {
+            return;
+        }
+    }
+    $passwords[$alias] = expect_password("Provide password for '$alias':");
+
+    save_passwords($fileWithPasswords, $passwords, $masterPass);
+}
+
+function remove_password(string $fileWithPasswords, string $alias): void
+{
+    $masterPass = expect_password("Master password: ");
+    $passwords = get_passwords($fileWithPasswords, $masterPass);
+    if (isset($passwords[$alias])) {
+        if (!get_confirmation("Are you sure you want to remove '{$alias}'? [y/n] ")) {
+            return;
+        }
+    } else {
+        die("There is no such password.\n");
+    }
+    unset($passwords[$alias]);
+    save_passwords($fileWithPasswords, $passwords, $masterPass);
+}
+
+function save_passwords(string $fileWithPasswords, array $passwords, string $masterPass): void
+{
+    file_put_contents($fileWithPasswords, encrypt(json_encode($passwords), $masterPass));
+}
+
+function get_confirmation(string $msg): bool
+{
+    $response = strtolower(readline($msg));
+    if ($response === 'y') return true;
+    if ($response === 'n') return false;
+
+    return get_confirmation($msg);
+}
+
+function get_aliases(string $fileWithPasswords): array
+{
+    return array_keys(get_passwords($fileWithPasswords, expect_password("Master password: ")));
+}
+
+function change_master_pass(string $fileWithPasswords): void
+{
+    $passwords = get_passwords($fileWithPasswords, expect_password("Master password: "));
+    save_passwords($fileWithPasswords, $passwords, expect_password("Provide new master-password:"));
+}
+
 
 function get_passwords(string $fileWithPasswords, string $masterPass)
 {
     $content = file_get_contents($fileWithPasswords);
-    if (empty($content)){
+    if (empty($content)) {
         return [];
     }
     $passwords = json_decode(decrypt($content, $masterPass), true);
-    if ($passwords === null){
+    if ($passwords === null) {
         die("Wrong password.\n");
     }
     return $passwords;
 }
 
+# content = brown fox jumps over the lazy dog
+# map     = testtesttesttesttesttesttesttestt
+# result  =.s;af,lpwemeiasjdpqw;
 
-function decrypt(string $content, string $key):string{
+
+function decrypt(string $content, string $key): string
+{
     $decrypted = '';
     $map = str_pad('', strlen($content), $key);
     foreach (str_split($content) as $index => $symbol) {
@@ -29,7 +93,8 @@ function decrypt(string $content, string $key):string{
     return $decrypted;
 }
 
-function encrypt(string $content, string $key):string{
+function encrypt(string $content, string $key): string
+{
     $encrypted = '';
     $map = str_pad('', strlen($content), $key);
     foreach (str_split($content) as $index => $symbol) {
@@ -37,4 +102,21 @@ function encrypt(string $content, string $key):string{
     }
 
     return $encrypted;
+}
+
+
+function expect_password(string $msg): string
+{
+    // turn off echo
+    `/bin/stty -echo`;
+    $password = readline($msg);
+    // turn echo back on
+    `/bin/stty echo`;
+
+    print "\n";
+
+    if (empty($password)) {
+        return expect_password($msg);
+    }
+    return $password;
 }
--- passman.php
+++ passman.php
@@ -1,12 +1,17 @@
 <?php
+
+require "functions.php";
+
 if ($argc < 2){
     die(<<<USAGE
-    Usage:
-        passman set <alias>
-        passman get <alias>
-        #passman copy <alias>
-        #passman remove <alias>
-        #passman change-masterpass
+Usage:
+    passman set <alias>
+    passman get <alias>
+    passman list
+    passman copy <alias>
+    passman remove <alias>
+    #passman change-masterpass
+
 USAGE
     );
 }
@@ -17,7 +22,9 @@ USAGE
 
 [$file, $command, $alias] = array_pad($argv, 3, '');
 
-$fileWithPasswords = getenv('HOME') . '/.config/passman';
+//$fileWithPasswords = getenv('HOME') . '/.config/passman';
+
+$fileWithPasswords = './.passman';
 if (!file_exists($fileWithPasswords)){
     touch($fileWithPasswords);
 }
@@ -25,21 +32,29 @@ if (!file_exists($fileWithPasswords)){
 
 switch ($command){
     case 'set':
-//        set_password($fileWithPasswords, $alias);
+        set_password($fileWithPasswords, $alias);
         die;
     case 'get':
-        print "Passowrd:" . get_password($fileWithPasswords, $alias)."\n";
+        print "Password:" . get_password($fileWithPasswords, $alias)."\n";
         die;
     case 'list':
-//        $aliases = get_aliases($fileWithPasswords);
-//        foreach ($aliases as $alias){
-//            print " - $alias\n";
-//        }
-        die;
-    case 'copy':
-//        get_password($fileWithPasswords, $alias);
+        $aliases = get_aliases($fileWithPasswords);
+        print "Available passwords in the vault:\n";
+        foreach ($aliases as $alias){
+            print " - $alias\n";
+        }
         die;
     case 'remove':
-//        get_password($fileWithPasswords, $alias);
+        remove_password($fileWithPasswords, $alias);
         die;
+    case 'copy':
+        $password = get_password($fileWithPasswords, $alias);
+        shell_exec("echo \"$password\" | xsel -ib");
+        echo "Copied.\n";
+        die;
+    case 'change-masterpass':
+        change_master_pass($fileWithPasswords);
+        die("Done.\n");
+    default:
+        die("No such command.\n");
 }
