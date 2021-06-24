16d13b2422e98b834c8c827afd9f6070138df048
Alexandr Harabara <alexandr.harabara@bitpanda.com>
2021-06-24 18:28:15 +0300

    before-tests

--- functions.php
+++ functions.php
@@ -120,3 +120,58 @@ function expect_password(string $msg): string
     }
     return $password;
 }
+
+
+
+function execute($command, $alias): void
+{
+    $fileWithPasswords = './.passman';
+    if (!file_exists($fileWithPasswords)) {
+        touch($fileWithPasswords);
+    }
+
+
+    switch ($command) {
+        case 'set':
+            set_password($fileWithPasswords, $alias);
+            die;
+        case 'get':
+            print "Password:" . get_password($fileWithPasswords, $alias) . "\n";
+            die;
+        case 'list':
+            $aliases = get_aliases($fileWithPasswords);
+            print "Available passwords in the vault:\n";
+            foreach ($aliases as $alias) {
+                print " - $alias\n";
+            }
+            die;
+        case 'remove':
+            remove_password($fileWithPasswords, $alias);
+            die;
+        case 'copy':
+            $password = get_password($fileWithPasswords, $alias);
+            shell_exec("echo \"$password\" | xsel -ib");
+            echo "Copied.\n";
+            die;
+        case 'change-masterpass':
+            change_master_pass($fileWithPasswords);
+            die("Done.\n");
+        default:
+            die("No such command.\n");
+    }
+}
+
+
+function get_usage(): string
+{
+    return <<<USAGE
+Usage:
+    passman set <alias>
+    passman get <alias>
+    passman list
+    passman copy <alias>
+    passman remove <alias>
+    #passman change-masterpass
+
+USAGE;
+}
--- passman.php
+++ passman.php
@@ -1,19 +1,9 @@
+#!/usr/bin/env php
 <?php
+require __DIR__ . "/vendor/autoload.php";
 
-require "functions.php";
-
-if ($argc < 2){
-    die(<<<USAGE
-Usage:
-    passman set <alias>
-    passman get <alias>
-    passman list
-    passman copy <alias>
-    passman remove <alias>
-    #passman change-masterpass
-
-USAGE
-    );
+if ($argc < 2) {
+    die(get_usage());
 }
 
 # restricted to
@@ -24,37 +14,4 @@ USAGE
 
 //$fileWithPasswords = getenv('HOME') . '/.config/passman';
 
-$fileWithPasswords = './.passman';
-if (!file_exists($fileWithPasswords)){
-    touch($fileWithPasswords);
-}
-
-
-switch ($command){
-    case 'set':
-        set_password($fileWithPasswords, $alias);
-        die;
-    case 'get':
-        print "Password:" . get_password($fileWithPasswords, $alias)."\n";
-        die;
-    case 'list':
-        $aliases = get_aliases($fileWithPasswords);
-        print "Available passwords in the vault:\n";
-        foreach ($aliases as $alias){
-            print " - $alias\n";
-        }
-        die;
-    case 'remove':
-        remove_password($fileWithPasswords, $alias);
-        die;
-    case 'copy':
-        $password = get_password($fileWithPasswords, $alias);
-        shell_exec("echo \"$password\" | xsel -ib");
-        echo "Copied.\n";
-        die;
-    case 'change-masterpass':
-        change_master_pass($fileWithPasswords);
-        die("Done.\n");
-    default:
-        die("No such command.\n");
-}
+execute($command, $alias);
